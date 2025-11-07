65e73c6be881cec2493e05d142e4f4e2b935738b
Ben Allfree <ben@benallfree.com>
2025-11-07 06:17:27 -0800

    wip

--- src/modules/Modules.cpp
+++ src/modules/Modules.cpp
@@ -107,7 +107,7 @@
 #include "modules/DropzoneModule.h"
 #endif
 
-#include "modules/LoBBSModule/LoBBSModule.h"
+#include "../../community/lobbs/LoBBSModule.h"
 
 /**
  * Create module instances here.  If you are adding a new module, you must 'new' it here (or somewhere else)
